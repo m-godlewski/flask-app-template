d8a29a14afeff4bd76f6a32cebcf94c16ccdb8ae
Mateusz <mateusz@mysocialseller.com>
2020-11-03 19:20:33 +0100

    template updated

--- core/routes.py
+++
@@ -1,7 +0,0 @@
-from core import app, controller, logging
-from flask import jsonify
-
-@app.route('/', methods=['GET'])
-def index():
-    logging.info('LOGGING -> INDEX')
-    return jsonify('Index'), 200
--- module_name.py
+++
@@ -1,5 +0,0 @@
-import core, config
-
-if __name__ == '__main__':  
-    core.app.debug=config.DEBUG
-    core.app.run(host='0.0.0.0', port=config.PORT)
